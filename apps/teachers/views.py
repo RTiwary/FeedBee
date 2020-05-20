42038c547c6f27a7f1b0fb86e4917acb6210fef1
rtiwary <tiwaryronit@gmail.com>
2020-05-19 20:48:23 -0400

    Revert "will this work?"
    This reverts commit 1eb3a081bf7cb21881a03878e5477e1874fceb0b.

--- apps/teachers/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
-
-# Create your views here.
-@user_passes_test(lambda user: True if user.get_access('bulk_email') else False, login_url='/user/login')
