5b8bc00980b4f25f7d8da2351e4f67e885889b39
rtiwary <tiwaryronit@gmail.com>
2020-05-31 22:11:22 -0400

    pushed other files

---
+++ apps/teachers/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from apps.teachers import views
+
+urlpatterns = [
+    path('teacher_dashboard/', views.teacher_dashboard, name="teacher_dashboard"),
+    path('add_class/', views.add_class, name="add_class"),
+    path('view_classes/', views.view_classes, name="view_classes"),
+    path('suggest_feature/', views.suggest_feature, name="suggest_feature"),
+    path('logout/', views.logout_request, name="logout"),
+]
