ca6718490118508a32887c120c7737903a3e2968
edwhuang <edwhuang@umich.edu>
2020-06-02 00:27:35 -0400

    Added reset password form

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,6 @@ urlpatterns = [
     path('login/', views.login_request, name='login'),
     path('register_student/', views.register_student, name="register_student"),
     path('register_teacher/', views.register_teacher, name="register_teacher"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password_form.html')),
+    #path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password_form.html')),
 ]
