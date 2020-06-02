cf4c4b6cc43b826f4b9729b4c19105b3130a6042
edwhuang <edwhuang@umich.edu>
2020-06-02 01:24:39 -0400

    added all password reset html files

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('register_student/', views.register_student, name="register_student"),
     path('register_teacher/', views.register_teacher, name="register_teacher"),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password_form.html')),
-    #path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password_form.html')),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/reset_password_done.html')),
+    path('password_reset_confirm/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/reset_password_confirm.html')),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/reset_password_complete.html')),
 ]
