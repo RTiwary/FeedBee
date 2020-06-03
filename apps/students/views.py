7f56290e8c7e6ca27d083abc130e528e3b6e10c9
raeed <raeed@umich.edu>
2020-06-03 19:31:27 -0400

    take_survey template/view and student navbar

--- apps/students/views.py
+++ apps/students/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from apps.teachers.models import CheckboxQuestion, TextQuestion, BooleanQuestion, MultipleChoiceQuestion, Survey
+
+
+def take_quiz(request, pk):
+    quiz = get_object_or_404(Survey, pk=pk)
+    student = request.user.student
+
+    boolQuestions = BooleanQuestion.objects.filter(quiz=quiz)
+    mcQuestions = MultipleChoiceQuestion.objects.filter(quiz=quiz)
+    txtQuestions = TextQuestion.objects.filter(quiz=quiz)
+    cbQuestions = CheckboxQuestion.object.filter(quiz=quiz)
+    total_questions = len(boolQuestions) + len(mcQuestions) + len(txtQuestions) + len(cbQuestions)
+
+
+    return render(request, 'classroom/students/take_quiz_form.html', {
+        'quiz': quiz,
+        'total_questions': total_questions,
+        'boolQuestions': boolQuestions,
+        'txtQuestions': txtQuestions,
+        'mcQuestions': mcQuestions,
+        'cbQuestions': cbQuestions
+    })
