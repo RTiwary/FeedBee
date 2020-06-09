4b926dd3ac5832f60054ce0083f3c425d3dd9f30
Cindy <cindygu@umich.edu>
2020-06-08 21:36:02 -0400

    changed password of postgres back

--- school/settings.py
+++ school/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'new_school',
         'USER': 'postgres',
-        'PASSWORD': 'Celtics1',
+        'PASSWORD': 'Pistons1',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
