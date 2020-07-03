31ca7da00725271683defd87002ee5911bb350e5
raeed <raeed@umich.edu>
2020-07-03 15:04:22 -0400

    settings

--- school/settings.py
+++ school/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'new_school',
-        'USER': 'postgres',
-        'PASSWORD': 'Pistons1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_school',
     }
 }
 
