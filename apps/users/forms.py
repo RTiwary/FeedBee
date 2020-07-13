44ebe6ed50b4a1d1ad5f20e6689b90bcdf50c088
edwhuang <edwhuang@umich.edu>
2020-07-12 20:01:31 -0400

    Add documentation for user registration and change password views

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -2,10 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-
+'''
+Form for registering a new user
+'''
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
-    
     class Meta:
 	    model = User
 	    fields = ["username", "email", "password1", "password2"]
