f4d3a6c9108ddcbe99adf233521e990e448da539
rtiwary <tiwaryronit@gmail.com>
2020-08-08 21:06:15 -0400

    test with debug true

--- school/settings.py
+++ school/settings.py
@@ -30,7 +30,7 @@ def get_env_variable(var_name):
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://classbop.herokuapp.com/', 'http://127.0.0.1:8000/']
 
@@ -95,18 +95,19 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-if DEBUG:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'new_school',
-        'USER': 'postgres',
-        'PASSWORD': 'Pistons1',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# if DEBUG:
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'new_school',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Pistons1',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# else:
+#     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 CACHES = {
